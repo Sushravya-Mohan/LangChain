95ae99b8d3f4c712a1d20a66c67fcc90fbde9da8
Sushravya <sushravya.mohan@gmail.com>
2024-12-21 21:48:48 +0530

    handled pytube download issue

--- 12-TextSummarization/app.py
+++ 12-TextSummarization/app.py
@@ -1,10 +1,16 @@
+import os
 import validators
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
-from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Streamlit app
 st.set_page_config(
@@ -14,10 +20,14 @@ st.title("🦜 LangChain: Summarize Text from YT or Website")
 st.subheader("Summarize URL")
 
 # Get the Groq API key and url to be summarized
-with st.sidebar:
-    groq_api_key = st.text_input("Groq API Key", value="", type="password")
+# with st.sidebar:
+#     groq_api_key = st.text_input("Groq API Key", value="", type="password")
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+llm = ChatGroq(groq_api_key=groq_api_key, model="llama-3.3-70b-versatile")
 
 generic_url = st.text_input("URL", label_visibility="collapsed")
+urls = [generic_url]
 
 prompt_template = """
 Provide a summary of the following content in 300 words:
@@ -26,26 +36,28 @@ Content: {text}
 
 prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
 
+save_dir = "/Downloads/YouTube"
+
+
 if st.button("Summarize the content from YT or Website"):
     # Validate all the inputs
-    if not groq_api_key.strip() or not generic_url.strip():
+    if not generic_url.strip():
         st.error("Please provide the information to get started.")
     elif not validators.url(generic_url):
         st.error("Please enter a valid URL. It may be a YT video or website URL.")
 
     else:
         # Gemma model using Groq API
-        llm = ChatGroq(groq_api_key=groq_api_key, model="gemma2-9b-it")
         try:
             with st.spinner("Waiting......."):
                 # Loading the website or YT video
                 if "youtube.com" in generic_url:
-                    loader = YoutubeLoader.from_youtube_url(
-                        generic_url, add_video_info=True
+                    loader = GenericLoader(
+                        YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParser()
                     )
                 else:
                     loader = UnstructuredURLLoader(
-                        urls=[generic_url],
+                        urls=urls,
                         ssl_verify=False,
                         headers={
                             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
@@ -57,7 +69,7 @@ if st.button("Summarize the content from YT or Website"):
 
                 # Chain for summarization
                 chain = load_summarize_chain(llm=llm, chain_type="stuff", prompt=prompt)
-                output_summary = chain.run(docs)
+                output_summary = chain.invoke(docs)
 
                 st.success(output_summary)
         except Exception as e:
